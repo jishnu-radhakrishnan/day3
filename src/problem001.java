0a1c80ee08ea603fab44b011b20652e55ce5767a
jishnu-radhakrishnan <jishnurk10@gmail.com>
2022-04-18 22:43:06 +0530

    added array display

---
+++ src/problem001.java
@@ -0,0 +1,13 @@
+//1) Java Program to print the elements of an array
+
+public class problem001 {
+    public static void main(String[] args){
+        int[] arr = {10, 20, 30, 40};
+        for(int i=0;i<=4;i++)
+        {
+            int x=arr[i];
+            System.out.println(x);
+        }
+    }
+
+}
